1a06669b92d8e0d8c4b9c947b911fe1fd90bf78a
MSNP1381 <mohamadnematpoor@gmail.com>
2022-11-18 12:51:51 +0330

    restaurant changed

---
+++ Resturant-managment/Migrations/20221118092114_ff.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resturant_managment.Migrations
+{
+    public partial class ff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Cities_IdentityId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Avg",
+                table: "Restaurant",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdentityId",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Cities_IdentityId",
+                table: "AspNetUsers",
+                column: "IdentityId",
+                principalTable: "Cities",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Cities_IdentityId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Avg",
+                table: "Restaurant");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdentityId",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Cities_IdentityId",
+                table: "AspNetUsers",
+                column: "IdentityId",
+                principalTable: "Cities",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
