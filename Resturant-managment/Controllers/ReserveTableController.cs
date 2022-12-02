c1c16bdab8e38c597b517cac6b6113d09f2929a3
MSNP1381 <mohamadnematpoor@gmail.com>
2022-12-02 17:02:36 +0330

    GetActiveReserveTablesByRestaurant added

--- Resturant-managment/Controllers/ReserveTableController.cs
+++ Resturant-managment/Controllers/ReserveTableController.cs
@@ -9,20 +9,29 @@ namespace Resturant_managment.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ReserveTableController : ControllerBase
     {
         private readonly RmDbContext _db;
         private readonly UserManager<RestaurantIdentity> _userManager;
-
-        public  ReserveTableController(RmDbContext db, UserManager<RestaurantIdentity> userManager)
+        public ReserveTableController(RmDbContext db, UserManager<RestaurantIdentity> userManager)
         {
             _db = db;
             _userManager = userManager;
+
         }
 
+        private RestaurantIdentity GetUser()
+        {
+            var email = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+            return _userManager.FindByEmailAsync(email).Result;
+        }
         [HttpPost]
         public ActionResult Post(ReserveTable r)
         {
+            var user = GetUser();
+            r.RestaurantIdentity = user;
+            r.RestaurantIdentityId = user.Id;
             _db.Add(r);
             _db.SaveChanges();
             return Ok(r.id);
@@ -48,50 +57,58 @@ namespace Resturant_managment.Controllers
         [HttpPut]
         public ActionResult Put(ReserveTable r)
         {
+            var user = GetUser();
+            r.RestaurantIdentity = user;
+            r.RestaurantIdentityId = user.Id;
             if (r.id == 0) return NotFound();
             _db.Update(r);
             _db.SaveChanges();
             return Ok(r.id);
         }
-        [Authorize]
         [HttpGet("GetUserActiveReserveTables")]
         public async Task<List<ReserveTable>> GetReserveTable()
         {
 
-            var email = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
-            var user = await _userManager.FindByEmailAsync(email);
-             var result=_db.ReserveTables.Where(x => x.RestaurantIdentityId == user.Id)
+            var user = GetUser();
+            var result = _db.ReserveTables.Where(x => x.RestaurantIdentityId == user.Id)
                 .Where(x =>
-            
-                (x.ReserveTime.ReserveTime.AddHours(x.ExpireHours)>=DateTime.Now)
+
+                (x.ReserveTime.ReserveTime.AddHours(x.ExpireHours) >= DateTime.Now)
             );
-          
-            return result.ToList() ;
+
+            return result.ToList();
         }
 
         [HttpGet("GetUserActiveReserveTablesByRestaurant")]
-        public async Task<List<ReserveTable>> GetUserReserveTableByRestaurant(int restaurantid)
+        public List<ReserveTable> GetUserReserveTableByRestaurant(int restaurantid)
         {
 
-            var email = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
-            var user = await _userManager.FindByEmailAsync(email);
+            var user = GetUser();
+
             var result = _db.ReserveTables.Where(x => x.RestaurantIdentityId == user.Id)
                .Where(x =>
 
                (x.ReserveTime.ReserveTime.AddHours(x.ExpireHours) >= DateTime.Now)
-           ).Where(x => x.Table.RestaurantId==restaurantid);
+           ).Where(x => x.Table.RestaurantId == restaurantid);
 
             return result.ToList();
         }
 
         [HttpGet("GetReserveTablesByRestaurant")]
-        public async Task<List<ReserveTable>> GetReserveTableByRestaurant(int restaurantid)
+        public List<ReserveTable> GetReserveTableByRestaurant(int restaurantid)
         {
-
             var result = _db.ReserveTables.Where(x => x.Table.RestaurantId == restaurantid)
-                .Where(x =>check(x));
-
+                .Where(x => check(x));
             return result.ToList();
+        }
+        [HttpGet("GetActiveReserveTablesByRestaurant")]
+        public ActionResult<List<RestaurantTable>> GetActiveReserveTablesByRestaurant(int restauranId, DateTime from ,DateTime to) {
+            var result = _db.RestaurantTables.Where(x => x.ReserveTables
+              .All(y => y.ReserveTime.ReserveTime > from
+              &&
+              y.ReserveTime.ReserveTime.AddHours(y.ExpireHours) < to)).ToList();
+            return result;
+
         }
         private bool check(ReserveTable x)
         {
@@ -99,16 +116,12 @@ namespace Resturant_managment.Controllers
             var e = DateTime.Today.AddDays(1);
             return (x.ReserveTime.ReserveTime >= s && x.ReserveTime.ReserveTime <= e);
         }
-        [Authorize]
         [HttpGet("GetUserReserveTables")]
         public async Task<List<ReserveTable>> GetUserReserveTableHistory()
         {
 
-            var email = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
-            var user = await _userManager.FindByEmailAsync(email);
-            var result = _db.ReserveTables.Where(x => x.RestaurantIdentityId == user.Id)
-
-           ;
+            var user = GetUser();
+            var result = _db.ReserveTables.Where(x => x.RestaurantIdentityId == user.Id);
 
             return result.ToList();
         }
