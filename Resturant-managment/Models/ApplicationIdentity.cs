059e3884cd9d2ed699fc10b27583da704b2eec4f
Reyhashem <115505819+Reyhashem@users.noreply.github.com>
2022-12-13 14:15:03 +0330

    employee

--- Resturant-managment/Models/ApplicationIdentity.cs
+++ Resturant-managment/Models/ApplicationIdentity.cs
@@ -26,15 +26,23 @@ public class RestaurantIdentity : IdentityUser
     public List<String> RoleName { get; set; }
 
 }
+public enum jobtype
+{   
+    fulltime, parttime
+}
 public class Employee :BaseClass
 {
+    public int earning { get; set; }
+    public DateTime start { get; set; }
+    public jobtype? jobtype { get; set; }
+    public string position { get; set; }
     public virtual List<EntranceMangment>? entranceMangments { get; set; }
     public int restaurantid { get; set; }
     [ForeignKey("restaurantid")]
-    public virtual Restaurant Restaurant { get; set; }
+    public virtual Restaurant? Restaurant { get; set; }
 
     
-    public string identityid { get; set; }
+    public string? identityid { get; set; }
     [ForeignKey("identityid")]
-    public virtual RestaurantIdentity RestaurantIdentity { get; set; }
+    public virtual RestaurantIdentity? RestaurantIdentity { get; set; }
 }
