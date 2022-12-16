bb57a01b52178a106b6d1e97174247bf4effb793
MSNP1381 <mohamadnematpoor@gmail.com>
2022-12-16 17:19:56 +0330

    signup pic is optional

--- Resturant-managment/Controllers/TagController.cs
+++ Resturant-managment/Controllers/TagController.cs
@@ -31,7 +31,7 @@ namespace Resturant_managment.Controllers
         {
             return _db.Tags.Find(id);
         }
-        public ActionResult<List<Tag>> GetUniqueTagsByCity(int cityId)
+        public ActionResult<List<string>> GetUniqueTagsByCity(int cityId)
         {
             var result = _db.Tags.Where(r => r == null ? false : r.Restaurant.CityId == cityId).GroupBy(r => r.value).Select(t => t.Key).ToList();
             return Ok(result);
