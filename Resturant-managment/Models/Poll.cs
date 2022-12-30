4cae40eda93718fa5adeba1bfc5970724755706c
Reyhashem <115505819+Reyhashem@users.noreply.github.com>
2022-12-30 20:54:11 +0330

    fix poll

--- Resturant-managment/Models/Poll.cs
+++ Resturant-managment/Models/Poll.cs
@@ -6,18 +6,18 @@ namespace Resturant_managment.Models
     public class Poll:BaseClass
     {
        
-        public int score1 { get; set; }
-        public int score2 { get; set; }
-        public int score3 { get; set; }
-        public int score4 { get; set; }
-        public int score5 { get; set; }
-        public int score6 { get; set; }
-        public int score7 { get; set; }
+        public double score1 { get; set; }
+        public double score2 { get; set; }
+        public double score3 { get; set; }
+        public double score4 { get; set; }
+        public double score5 { get; set; }
+        public double score6 { get; set; }
+        public double score7 { get; set; }
        
 
         public int restaurantid { get; set; }
         [ForeignKey("restaurantid")]
-        public virtual Restaurant Restaurant { get; set; }
+        public virtual Restaurant? Restaurant { get; set; }
 
 
     }
