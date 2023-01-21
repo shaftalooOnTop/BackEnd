4dd880fb101478e6eb5dffc3d030c714dee5976f
Reyhashem <115505819+Reyhashem@users.noreply.github.com>
2023-01-21 10:20:26 +0330

    a

---
+++ Resturant-managment/Migrations/20230121064953_g21111.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resturant_managment.Migrations
+{
+    public partial class g21111 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
